bee6950a413e60bc63497031ab661bebdb5c2c7e
Malay Pandey <malayp717@gmail.com>
2023-12-20 02:26:15 +0530

    Added requirements.txt

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-base_dir = 'C:/Users/malay/OneDrive/Desktop/CSE/Thesis/pm2.5/'
+base_dir = '/home/malay/pm25/pm2.5/'
 data_dir = f'{base_dir}/data/'
 data_bihar = f'{data_dir}/Bihar/'
 bihar_data_419 = f'{data_bihar}/Bihar_419_data.pkl'
