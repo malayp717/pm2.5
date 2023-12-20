26c39680fd4af2c48705f522442904aad1605ff4
Malay Pandey <malayp717@gmail.com>
2023-12-20 16:23:12 +0530

    Server Configured

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 base_dir = '/home/malay/pm25/pm2.5/'
-data_dir = f'{base_dir}/data/'
-data_bihar = f'{data_dir}/Bihar/'
+data_dir = f'/hdd/malay/'
+data_bihar = f'{data_dir}/bihar/'
 bihar_data_419 = f'{data_bihar}/Bihar_419_data.pkl'
-model_dir = f'{base_dir}/models/'
-plot_dir = f'{base_dir}/plots/'
+model_dir = f'{data_dir}/models/'
+plot_dir = f'{data_dir}/plots/'
 bihar_plot_dir = f'{plot_dir}/bihar/'
 GRID_SIZE = 800
 AREA_BIHAR = 94163
