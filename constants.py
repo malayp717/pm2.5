168eb9976fc8170771e08079dfadf4e7e31f3fb1
Malay Pandey <malayp717@gmail.com>
2023-12-21 21:21:34 +0530

    New Data Loaded

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 base_dir = '/home/malay/pm25/pm2.5/'
-data_dir = f'/hdd/malay/'
-data_bihar = f'{data_dir}/bihar/'
+data_dir = f'/hdd/malay'
+data_bihar = f'{data_dir}/bihar'
 bihar_data_419 = f'{data_bihar}/Bihar_419_data.pkl'
 model_dir = f'{data_dir}/models/'
 plot_dir = f'{data_dir}/plots/'
