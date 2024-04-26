e92a2fbfd43ed0ffb5cf831c47d2a1beab7d3a7c
Malay Pandey <malayp717@gmail.com>
2024-04-27 01:29:31 +0530

    KnowAir changes added

--- dataset.py
+++ dataset.py
@@ -1,7 +1,9 @@
-import numpy as np
-import yaml
 import os
 import sys
+import yaml
+import numpy as np
+from datetime import datetime
+from dataset.TemporalDataset import TemporalDataset
 
 proj_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(proj_dir)
@@ -10,9 +12,49 @@ config_fp = os.path.join(proj_dir, 'config.yaml')
 with open(config_fp, 'r') as f:
     config = yaml.safe_load(f)
 
-class TemporalDataset():
-    def __init__(self, data_fp, locations_fp):
-        pass
+# ------------- Config parameters start ------------- #
+data_dir = config['filepath']['data_dir']
+model_dir = config['filepath']['model_dir']
+bihar_pkl_fp = data_dir + config['filepath']['bihar_pkl_fp']
+bihar_npy_fp = data_dir + config['filepath']['bihar_npy_fp']
+locations_fp = data_dir + config['filepath']['locations_fp']
+bihar_map_fp = data_dir + config['filepath']['bihar_map_fp']
+
+batch_size = int(config['train']['batch_size'])
+epochs = int(config['train']['epochs'])
+forecast_window = int(config['train']['forecast_window'])
+lr = float(config['train']['lr'])
+
+update = int(config['dataset']['update'])
+data_start = config['dataset']['data_start']
+data_end = config['dataset']['data_end']
+
+train_start = config['split']['train_start']
+train_end = config['split']['train_end']
+val_start = config['split']['val_start']
+val_end = config['split']['val_end']
+test_start = config['split']['test_start']
+test_end = config['split']['test_end']
+# ------------- Config parameters end   ------------- #
 
 if __name__ == '__main__':
-    pass
+
+    train_duration = ((datetime(*train_end) - datetime(*train_start)).days + 1) * (24//update)
+    val_duration = ((datetime(*val_end) - datetime(*val_start)).days + 1) * (24//update)
+    test_duration = ((datetime(*test_end) - datetime(*test_start)).days + 1) * (24//update)
+
+    train_start_index = (datetime(*train_start) - datetime(*data_start)).days * (24//update)
+    val_start_index = (datetime(*val_start) - datetime(*data_start)).days * (24//update)
+    test_start_index = (datetime(*test_start) - datetime(*data_start)).days * (24//update)
+
+    train_data = TemporalDataset(bihar_npy_fp, forecast_window, train_start_index, train_duration)
+    val_data = TemporalDataset(bihar_npy_fp, forecast_window, val_start_index, val_duration)
+    test_data = TemporalDataset(bihar_npy_fp, forecast_window, test_start_index, test_duration)
+
+    print(train_duration, val_duration, test_duration, train_duration+val_duration+test_duration)
+    print(train_start_index, val_start_index, test_start_index)
+
+    print(len(train_data), len(val_data), len(test_data))
+    print(train_data.shape())
+    print(val_data.shape())
+    print(test_data.shape())
---
+++ dataset/TemporalDataset.py
@@ -0,0 +1,48 @@
+import numpy as np
+from torch.utils import data
+
+class TemporalDataset(data.Dataset):
+    def __init__(self, data_fp, forecast_window, start_index, duration):
+        self.forecast_window = forecast_window
+        self._process_data(data_fp, start_index, duration)
+
+    def _process_location(self, data, index_loc, start_index, duration):
+
+        idx_start, idx_end = start_index+duration-self.forecast_window, start_index+duration
+
+        features = data[idx_start: idx_end, index_loc, :-1]
+        pm25_hist = data[start_index:idx_start, index_loc, -1]
+        pm25 = data[idx_start:start_index+duration, index_loc, -1]
+
+        return np.float32(features), np.float32(pm25_hist), np.float32(pm25)
+        
+    def _process_data(self, data_fp, start_index, duration):
+
+        '''
+            Shape of the numpy array is [t, l, f]
+                t: total number of timestamps
+                l: total number of locations
+                f: total number of features (including pm25) for each observation
+        '''
+        data = np.load(data_fp)
+        num_locs = data.shape[1]
+
+        self.feature, self.pm25_hist, self.pm25 = [], [], []
+
+        for index_loc in range(num_locs):
+            feature, pm25_hist, pm25 = self._process_location(data, index_loc, start_index, duration)
+            self.feature.append(feature)
+            self.pm25_hist.append(pm25_hist)
+            self.pm25.append(pm25)
+
+        self.feature, self.pm25_hist, self.pm25 = np.float32(self.feature), np.float32(self.pm25_hist), np.float32(self.pm25)
+    
+    def shape(self):
+        print(f'Feature shape: {self.feature.shape} \t PM25_Hist shape: {self.pm25_hist.shape} \t PM25 shape: {self.pm25.shape}')
+
+    def __len__(self):
+        assert len(self.pm25_hist) == len(self.feature) == len(self.pm25)
+        return len(self.feature)
+
+    def __getitem__(self, idx):
+        return self.pm25_hist[idx], self.feature[idx], self.pm25[idx]
